d295f21df4309c96f0cb5ebc82eb146ade0103b4
Isaac Sánchez <123047038@upq.edu.mx>
2026-06-21 16:57:18 -0600

    Creación del script de la base de datos

---
+++ api/database/init.sql
@@ -0,0 +1,103 @@
+/*
+---------------------------------------------------
+| ESTRUCTURA DE LA BASE DE DATOS "little_caesars" |
+---------------------------------------------------
+*/
+
+-- CREAR ESQUEMA PARA LOS TABLAS DE TIPO CATÁLOGO
+create schema if not exists lcs_cat;
+-- CREAR ESQUEMA PARA LAS TABLAS DE PRODUCTOS
+create schema if not exists lcs_pro;
+
+-- BORRAR LAS TABLAS EN ORDEN INVERSO PARA EVITAR ERRORES DE LLAVE FORÁNEA
+drop table if exists lcs_pro.combos_productos;
+drop table if exists lcs_pro.combos;
+drop table if exists lcs_pro.producto_ingredientes;
+drop table if exists lcs_pro.productos;
+drop table if exists lcs_cat.categorias_productos;
+drop table if exists lcs_cat.ingredientes;
+drop table if exists lcs_cat.unidades_medida;
+
+
+/*
+---------------------------
+| TABLAS DE TIPO CATÁLOGO |
+---------------------------
+*/
+
+-- ALMACENAR LAS UNIDADES DE MEDIDA DE LOS INGREDIENTES DE LOS PRODUCTOS
+create table lcs_cat.unidades_medida(
+    id_unidad_medida int generated always as identity primary key,
+    nombre varchar(20) unique not null
+);
+
+-- ALMACENAR LOS INGREDIENTES DE LOS PRODUCTOS
+create table lcs_cat.ingredientes(
+    id_ingrediente int generated always as identity primary key,
+    id_unidad_medida int not null,
+    nombre varchar(100) unique not null,
+    constraint fk_ingrediente_unidad_medida 
+        foreign key(id_unidad_medida) 
+        references lcs_cat.unidades_medida(id_unidad_medida)
+);
+
+-- ALMACENAR LAS CATEGORÍAS DE LOS PRODUCTOS
+create table lcs_cat.categorias_productos(
+    id_categoria int generated always as identity primary key,
+    nombre varchar(20) unique not null
+);
+
+
+/*
+----------------------
+| TABLAS PRINCIPALES |
+----------------------
+*/
+
+-- ALMACENAR LOS PRODUCTOS
+create table lcs_pro.productos(
+    id_producto int generated always as identity primary key,
+    id_categoria int not null,
+    nombre varchar(100) unique not null,
+    descripcion varchar(255),
+    constraint fk_producto_categoria 
+        foreign key(id_categoria) 
+        references lcs_cat.categorias_productos(id_categoria)
+);
+
+-- ALMACENAR LOS INGREDIENTES DE CADA PRODUCTO
+create table lcs_pro.producto_ingredientes(
+    id_producto int not null,
+    id_ingrediente int not null,
+    cantidad numeric(7, 2) not null check(cantidad > 0),
+    constraint fk_producto_ingrediente 
+        foreign key(id_producto) 
+        references lcs_pro.productos(id_producto) 
+        on delete cascade 
+        on update cascade,
+    constraint fk_ingrediente_producto 
+        foreign key(id_ingrediente) 
+        references lcs_cat.ingredientes(id_ingrediente),
+    primary key(id_producto, id_ingrediente)
+);
+
+-- ALMACENAR LOS NOMBRES DE LOS COMBOS DE LOS PRODUCTOS
+create table lcs_pro.combos(
+    id_combo int generated always as identity primary key,
+    nombre varchar(100) not null,
+    descripcion varchar(255)
+);
+
+-- ALMACENAR LOS COMBOS DE LOS PRODUCTOS
+create table lcs_pro.combos_productos(
+    id_combo int not null,
+    id_producto int not null,
+    cantidad smallint not null,
+    constraint fk_combo_producto 
+        foreign key(id_producto) 
+        references lcs_pro.productos(id_producto),
+    constraint fk_producto_combo 
+        foreign key(id_combo) 
+        references lcs_pro.combos(id_combo),
+    primary key(id_combo, id_producto)
+);
